7dcd579e12d3912346578d894ed23c9b5dbb98b9
kaisy84 <kaisy84@gmx.de>
2025-12-01 20:58:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function toggleMenu() {
     const nav = document.getElementById("nav");
 
-    if (nav.style.display === "flex") {
-        nav.style.display = "none";
-    } else {
-        nav.style.display = "flex";
-        nav.style.flexDirection = "column";
-    }
+    // Navigation ein-/ausblenden durch CSS-Klasse
+    nav.classList.toggle("open");
 }
