811000940eef301b4dace2450bb04779a6bc4978
kaisy84 <kaisy84@gmx.de>
2025-12-07 10:58:00 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,3 +2,24 @@ function toggleMenu() {
     const nav = document.getElementById("nav");
     nav.classList.toggle("open");
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector(".kontaktform");
+    const success = document.getElementById("success-message");
+
+    if (form) {
+        form.addEventListener("submit", function(e) {
+            e.preventDefault(); // verhindert Seitenreload
+
+            // Erfolgsmeldung anzeigen
+            success.style.display = "block";
+
+            // Formular leeren
+            form.reset();
+
+            // Meldung nach 6 Sekunden automatisch ausblenden
+            setTimeout(() => {
+                success.style.display = "none";
+            }, 6000);
+        });
+    }
+});
