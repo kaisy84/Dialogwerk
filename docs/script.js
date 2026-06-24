0f41e8b8f3aac7d17442514840c02a3484d479a2
kaisy84 <kaisy84@gmx.de>
2026-06-24 17:15:50 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,25 +1,3 @@
 function toggleMenu() {
-    const nav = document.getElementById("nav");
-    nav.classList.toggle("open");
+  document.getElementById("mainNav").classList.toggle("open");
 }
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector(".kontaktform");
-    const success = document.getElementById("success-message");
-
-    if (form) {
-        form.addEventListener("submit", function(e) {
-            e.preventDefault(); // verhindert Seitenreload
-
-            // Erfolgsmeldung anzeigen
-            success.style.display = "block";
-
-            // Formular leeren
-            form.reset();
-
-            // Meldung nach 6 Sekunden automatisch ausblenden
-            setTimeout(() => {
-                success.style.display = "none";
-            }, 6000);
-        });
-    }
-});
